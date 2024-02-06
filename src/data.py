197a87ab30a92715d8d51f5081c51aff5b2cf551
Kateřina Č <katterrina@aurora>
2024-02-06 17:07:57 +0100

    fixed Destrieux parcellation for domhof dataset, fixed enigma

--- src/data.py
+++ src/data.py
@@ -18,7 +18,7 @@ data_root = os.path.abspath(
 def path(relp):
     return os.path.join(data_root, os.path.normpath(relp))
 
-def load_ftract(parcellation,DKT_68=False):
+def load_ftract(parcellation):
     probability = np.loadtxt(f'../data/external/F-TRACT/{parcellation}/probability.txt.gz')
     amplitude = np.loadtxt(f'../data/external/F-TRACT/{parcellation}/amplitude__median.txt.gz')
     n_stim = np.loadtxt(f'../data/external/F-TRACT/{parcellation}/N_stimulations.txt.gz')
@@ -26,8 +26,11 @@ def load_ftract(parcellation,DKT_68=False):
 
     labels = np.loadtxt(f'../data/external/F-TRACT/{parcellation}/{parcellation}.txt', dtype=str)
 
-    if parcellation == "DKT":
-        roi = ['ctx-lh-bankssts','ctx-lh-caudalanteriorcingulate','ctx-lh-caudalmiddlefrontal','ctx-lh-corpuscallosum','ctx-lh-cuneus','ctx-lh-entorhinal','ctx-lh-fusiform','ctx-lh-inferiorparietal','ctx-lh-inferiortemporal','ctx-lh-isthmuscingulate','ctx-lh-lateraloccipital','ctx-lh-lateralorbitofrontal','ctx-lh-lingual','ctx-lh-medialorbitofrontal','ctx-lh-middletemporal','ctx-lh-parahippocampal','ctx-lh-paracentral','ctx-lh-parsopercularis','ctx-lh-parsorbitalis','ctx-lh-parstriangularis','ctx-lh-pericalcarine','ctx-lh-postcentral','ctx-lh-posteriorcingulate','ctx-lh-precentral','ctx-lh-precuneus','ctx-lh-rostralanteriorcingulate','ctx-lh-rostralmiddlefrontal','ctx-lh-superiorfrontal','ctx-lh-superiorparietal','ctx-lh-superiortemporal','ctx-lh-supramarginal','ctx-lh-frontalpole','ctx-lh-temporalpole','ctx-lh-transversetemporal','ctx-lh-insula','ctx-rh-bankssts','ctx-rh-caudalanteriorcingulate','ctx-rh-caudalmiddlefrontal','ctx-rh-corpuscallosum','ctx-rh-cuneus','ctx-rh-entorhinal','ctx-rh-fusiform','ctx-rh-inferiorparietal','ctx-rh-inferiortemporal','ctx-rh-isthmuscingulate','ctx-rh-lateraloccipital','ctx-rh-lateralorbitofrontal','ctx-rh-lingual','ctx-rh-medialorbitofrontal','ctx-rh-middletemporal','ctx-rh-parahippocampal','ctx-rh-paracentral','ctx-rh-parsopercularis','ctx-rh-parsorbitalis','ctx-rh-parstriangularis','ctx-rh-pericalcarine','ctx-rh-postcentral','ctx-rh-posteriorcingulate','ctx-rh-precentral','ctx-rh-precuneus','ctx-rh-rostralanteriorcingulate','ctx-rh-rostralmiddlefrontal','ctx-rh-superiorfrontal','ctx-rh-superiorparietal','ctx-rh-superiortemporal','ctx-rh-supramarginal','ctx-rh-frontalpole','ctx-rh-temporalpole','ctx-rh-transversetemporal','ctx-rh-insula']
+    if parcellation == "DKT" or parcellation == "Destrieux":
+        if parcellation == "DKT":
+            roi = ['ctx-lh-bankssts','ctx-lh-caudalanteriorcingulate','ctx-lh-caudalmiddlefrontal','ctx-lh-corpuscallosum','ctx-lh-cuneus','ctx-lh-entorhinal','ctx-lh-fusiform','ctx-lh-inferiorparietal','ctx-lh-inferiortemporal','ctx-lh-isthmuscingulate','ctx-lh-lateraloccipital','ctx-lh-lateralorbitofrontal','ctx-lh-lingual','ctx-lh-medialorbitofrontal','ctx-lh-middletemporal','ctx-lh-parahippocampal','ctx-lh-paracentral','ctx-lh-parsopercularis','ctx-lh-parsorbitalis','ctx-lh-parstriangularis','ctx-lh-pericalcarine','ctx-lh-postcentral','ctx-lh-posteriorcingulate','ctx-lh-precentral','ctx-lh-precuneus','ctx-lh-rostralanteriorcingulate','ctx-lh-rostralmiddlefrontal','ctx-lh-superiorfrontal','ctx-lh-superiorparietal','ctx-lh-superiortemporal','ctx-lh-supramarginal','ctx-lh-frontalpole','ctx-lh-temporalpole','ctx-lh-transversetemporal','ctx-lh-insula','ctx-rh-bankssts','ctx-rh-caudalanteriorcingulate','ctx-rh-caudalmiddlefrontal','ctx-rh-corpuscallosum','ctx-rh-cuneus','ctx-rh-entorhinal','ctx-rh-fusiform','ctx-rh-inferiorparietal','ctx-rh-inferiortemporal','ctx-rh-isthmuscingulate','ctx-rh-lateraloccipital','ctx-rh-lateralorbitofrontal','ctx-rh-lingual','ctx-rh-medialorbitofrontal','ctx-rh-middletemporal','ctx-rh-parahippocampal','ctx-rh-paracentral','ctx-rh-parsopercularis','ctx-rh-parsorbitalis','ctx-rh-parstriangularis','ctx-rh-pericalcarine','ctx-rh-postcentral','ctx-rh-posteriorcingulate','ctx-rh-precentral','ctx-rh-precuneus','ctx-rh-rostralanteriorcingulate','ctx-rh-rostralmiddlefrontal','ctx-rh-superiorfrontal','ctx-rh-superiorparietal','ctx-rh-superiortemporal','ctx-rh-supramarginal','ctx-rh-frontalpole','ctx-rh-temporalpole','ctx-rh-transversetemporal','ctx-rh-insula']
+        else:
+            roi = ['ctx_lh_G_and_S_frontomargin','ctx_lh_G_and_S_occipital_inf','ctx_lh_G_and_S_paracentral','ctx_lh_G_and_S_subcentral','ctx_lh_G_and_S_transv_frontopol','ctx_lh_G_and_S_cingul-Ant','ctx_lh_G_and_S_cingul-Mid-Ant','ctx_lh_G_and_S_cingul-Mid-Post','ctx_lh_G_cingul-Post-dorsal','ctx_lh_G_cingul-Post-ventral','ctx_lh_G_cuneus','ctx_lh_G_front_inf-Opercular','ctx_lh_G_front_inf-Orbital','ctx_lh_G_front_inf-Triangul','ctx_lh_G_front_middle','ctx_lh_G_front_sup','ctx_lh_G_Ins_lg_and_S_cent_ins','ctx_lh_G_insular_short','ctx_lh_G_occipital_middle','ctx_lh_G_occipital_sup','ctx_lh_G_oc-temp_lat-fusifor','ctx_lh_G_oc-temp_med-Lingual','ctx_lh_G_oc-temp_med-Parahip','ctx_lh_G_orbital','ctx_lh_G_pariet_inf-Angular','ctx_lh_G_pariet_inf-Supramar','ctx_lh_G_parietal_sup','ctx_lh_G_postcentral','ctx_lh_G_precentral','ctx_lh_G_precuneus','ctx_lh_G_rectus','ctx_lh_G_subcallosal','ctx_lh_G_temp_sup-G_T_transv','ctx_lh_G_temp_sup-Lateral','ctx_lh_G_temp_sup-Plan_polar','ctx_lh_G_temp_sup-Plan_tempo','ctx_lh_G_temporal_inf','ctx_lh_G_temporal_middle','ctx_lh_Lat_Fis-ant-Horizont','ctx_lh_Lat_Fis-ant-Vertical','ctx_lh_Lat_Fis-post','ctx_lh_Medial_wall','ctx_lh_Pole_occipital','ctx_lh_Pole_temporal','ctx_lh_S_calcarine','ctx_lh_S_central','ctx_lh_S_cingul-Marginalis','ctx_lh_S_circular_insula_ant','ctx_lh_S_circular_insula_inf','ctx_lh_S_circular_insula_sup','ctx_lh_S_collat_transv_ant','ctx_lh_S_collat_transv_post','ctx_lh_S_front_inf','ctx_lh_S_front_middle','ctx_lh_S_front_sup','ctx_lh_S_interm_prim-Jensen','ctx_lh_S_intrapariet_and_P_trans','ctx_lh_S_oc_middle_and_Lunatus','ctx_lh_S_oc_sup_and_transversal','ctx_lh_S_occipital_ant','ctx_lh_S_oc-temp_lat','ctx_lh_S_oc-temp_med_and_Lingual','ctx_lh_S_orbital_lateral','ctx_lh_S_orbital_med-olfact','ctx_lh_S_orbital-H_Shaped','ctx_lh_S_parieto_occipital','ctx_lh_S_pericallosal','ctx_lh_S_postcentral','ctx_lh_S_precentral-inf-part','ctx_lh_S_precentral-sup-part','ctx_lh_S_suborbital','ctx_lh_S_subparietal','ctx_lh_S_temporal_inf','ctx_lh_S_temporal_sup','ctx_lh_S_temporal_transverse','ctx_rh_G_and_S_frontomargin','ctx_rh_G_and_S_occipital_inf','ctx_rh_G_and_S_paracentral','ctx_rh_G_and_S_subcentral','ctx_rh_G_and_S_transv_frontopol','ctx_rh_G_and_S_cingul-Ant','ctx_rh_G_and_S_cingul-Mid-Ant','ctx_rh_G_and_S_cingul-Mid-Post','ctx_rh_G_cingul-Post-dorsal','ctx_rh_G_cingul-Post-ventral','ctx_rh_G_cuneus','ctx_rh_G_front_inf-Opercular','ctx_rh_G_front_inf-Orbital','ctx_rh_G_front_inf-Triangul','ctx_rh_G_front_middle','ctx_rh_G_front_sup','ctx_rh_G_Ins_lg_and_S_cent_ins','ctx_rh_G_insular_short','ctx_rh_G_occipital_middle','ctx_rh_G_occipital_sup','ctx_rh_G_oc-temp_lat-fusifor','ctx_rh_G_oc-temp_med-Lingual','ctx_rh_G_oc-temp_med-Parahip','ctx_rh_G_orbital','ctx_rh_G_pariet_inf-Angular','ctx_rh_G_pariet_inf-Supramar','ctx_rh_G_parietal_sup','ctx_rh_G_postcentral','ctx_rh_G_precentral','ctx_rh_G_precuneus','ctx_rh_G_rectus','ctx_rh_G_subcallosal','ctx_rh_G_temp_sup-G_T_transv','ctx_rh_G_temp_sup-Lateral','ctx_rh_G_temp_sup-Plan_polar','ctx_rh_G_temp_sup-Plan_tempo','ctx_rh_G_temporal_inf','ctx_rh_G_temporal_middle','ctx_rh_Lat_Fis-ant-Horizont','ctx_rh_Lat_Fis-ant-Vertical','ctx_rh_Lat_Fis-post','ctx_rh_Medial_wall','ctx_rh_Pole_occipital','ctx_rh_Pole_temporal','ctx_rh_S_calcarine','ctx_rh_S_central','ctx_rh_S_cingul-Marginalis','ctx_rh_S_circular_insula_ant','ctx_rh_S_circular_insula_inf','ctx_rh_S_circular_insula_sup','ctx_rh_S_collat_transv_ant','ctx_rh_S_collat_transv_post','ctx_rh_S_front_inf','ctx_rh_S_front_middle','ctx_rh_S_front_sup','ctx_rh_S_interm_prim-Jensen','ctx_rh_S_intrapariet_and_P_trans','ctx_rh_S_oc_middle_and_Lunatus','ctx_rh_S_oc_sup_and_transversal','ctx_rh_S_occipital_ant','ctx_rh_S_oc-temp_lat','ctx_rh_S_oc-temp_med_and_Lingual','ctx_rh_S_orbital_lateral','ctx_rh_S_orbital_med-olfact','ctx_rh_S_orbital-H_Shaped','ctx_rh_S_parieto_occipital','ctx_rh_S_pericallosal','ctx_rh_S_postcentral','ctx_rh_S_precentral-inf-part','ctx_rh_S_precentral-sup-part','ctx_rh_S_suborbital','ctx_rh_S_subparietal','ctx_rh_S_temporal_inf','ctx_rh_S_temporal_sup','ctx_rh_S_temporal_transverse'] 
 
         parcell_ids =  [i for i,label in enumerate(labels) if label in roi]
 
@@ -42,32 +45,9 @@ def load_ftract(parcellation,DKT_68=False):
 
 import matplotlib.pyplot as plt
 
-def load_enigma(reference_labels):
-    SC, sc_ctx_labels, _, _ = load_sc()
+def load_enigma():
+    SC, _, _, _ = load_sc()
     FC, _, _, _ = load_fc()
-
-    i,j = 0,0
-    mismatch_ids = []
-
-    while i < len(sc_ctx_labels) and j < len(reference_labels):
-        if sc_ctx_labels[i][2:] != reference_labels[j][7:]:
-            print(f"Labels {sc_ctx_labels[i]} and {reference_labels[j]} at index {i} do not match!\nAdding nan column/row for {reference_labels[j]}.")
-            mismatch_ids.append(i)
-            j+=1
-        else:
-            i+=1
-            j+=1
-
-    for i in mismatch_ids:
-        new_0 = np.empty((SC.shape[1],))
-        new_0.fill(np.nan)
-        SC = np.insert(SC,i,new_0, axis=0)
-        FC = np.insert(FC,i,new_0, axis=0)
-
-        new_1 = np.empty((SC.shape[0],))
-        new_1.fill(np.nan)
-        SC = np.insert(SC,i,new_1, axis=1)
-        FC = np.insert(FC,i,new_1, axis=1)
 
     SC_W = SC
     SC_L = np.where(SC==0.0,0,1/SC)
@@ -132,7 +112,6 @@ def glasser_roi_distances(n_roi,reference_labels):
     labels = []
 
     for h,hemisphere in [(0,'lh'),(1,'rh')]:
-        print(hemisphere)
         for i in range(n_roi//2):
             coor = surf[f'srf/{hemisphere}/vertices'][:][:,annot[f'annot/lIdx/{hemisphere}'][:].squeeze()==i+2] 
                 # +1 protože číslováno od 1, +1 protože první je '_MedialWall'
