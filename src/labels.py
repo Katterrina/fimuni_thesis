25947e33214beba7969a16cebf68ba6f6f223bbb
Kateřina Č <katterrina@aurora>
2024-05-05 20:29:10 +0200

    docstring

--- src/labels.py
+++ src/labels.py
@@ -3,8 +3,14 @@ import numpy as np
 
 from src.paths import path
 
-
 def ftract_compatible_glasser_labels():
+    """
+    Load Glasser ROI names in a form compatible with ROI names in F-TRACT, 
+    so it is possible to map these two to each other.
+
+    Returns:
+    list[str]: list od ROI names
+    """
     file = pd.read_csv(path('external/glasser_parcellation_centriods/HCP-MMP1_UniqueRegionList.csv'))  
 
     def premute_region_name(name):
@@ -18,8 +24,20 @@ def ftract_compatible_glasser_labels():
     return file["regionName"].apply(premute_region_name)
 
 def get_labels_from_file(centroids_file,label_column):
+    """
+    Load names of ROI from csv file, expecting the label_column
+    containing the labels as strings.
+
+    Parameters:
+    centroids_file (path): path to csv file with column containing labels
+    label_column (str): string indicating in which column in csv file are
+        the labels
+
+    Returns:
+    list[str]: labels
+    """
     df = pd.read_csv(centroids_file)
-    return df[label_column]
+    return list(df[label_column])
 
 def load_ftract_labels(parcellation):
     """
