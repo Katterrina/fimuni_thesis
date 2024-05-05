3b71323a36e43d86ad2d2bcc7abd9b6063536db7
Kateřina Č <katterrina@aurora>
2024-05-05 19:00:26 +0200

    docstring

--- src/centroids.py
+++ src/centroids.py
@@ -5,6 +5,16 @@ from src.paths import *
 from src.labels import *
 
 def roi_distances_from_centroids(centroids):
+    """
+    Calculate all distances between individual ROIs represented 
+    by centroids given as coordinates.
+    
+    Parameters:
+    centroids (2D np.array of shape (n_roi,3)): coordinates for each ROI
+
+    Returns:
+    2D np.array of shape (n_roi,n_roi): distances between all pair of ROIs
+    """
     n_roi = len(centroids)
 
     distances = np.zeros((n_roi,n_roi))
@@ -15,11 +25,34 @@ def roi_distances_from_centroids(centroids):
     return distances
 
 def get_centroids_from_file(centroids_file,geom_column):
+    """
+    Load coordinates of ROI centroids from csv file, expecting the geom_column
+    in the file contains coordinates in [x,y,z] form.
+
+    Parameters:
+    centroids_file (path): path to csv file with column containing centroid
+        coordinates in a list [x,y,z]
+    geom_column (str): string indicating in which column in csv file are
+        the coordinates of centroids
+
+    Returns:
+    centroids (2D np.array of shape (n_roi,3)): coordinates for each ROI
+    """
     df = pd.read_csv(centroids_file)
     centroids = np.stack(df[geom_column].apply(lambda x: np.fromstring(x[1:-1], dtype=float, sep=' ')).to_numpy(),axis=0)
     return centroids
 
 def load_glasser_centroids(ftract_labels=None):
+    """
+    Load coordinates of ROI centroids for Glasser parcellation.
+
+    Parameters:
+    ftract_labels (list[str]|None): list of F-TRACT labels if we want 
+        to reorder based on F-TRACT, if None do not reorder
+
+    Returns:
+    centroids (2D np.array of shape (n_roi,3)): coordinates for each ROI
+    """
     centroids_file = pd.read_csv(path('external/glasser_parcellation_centriods/HCP-MMP1_UniqueRegionList.csv'))   
     
     if ftract_labels is not None:
@@ -36,14 +69,29 @@ def load_glasser_centroids(ftract_labels=None):
     return centroids
 
 def glasser_roi_distances(ftract_labels=None):
+    """
+    Load matrix of Euclidean distances for ROIs in Glasser parcellation.
+    If ftract_labels list not None, reorder based on the labels.
 
+    Parameters:
+    ftract_labels (list[str]|None): list of F-TRACT labels if we want 
+        to reorder based on F-TRACT, if None do not reorder
+
+    Returns:
+    2D np.array of shape (n_roi,n_roi): distances between all pair of ROIs
+    """
     centroids = load_glasser_centroids(ftract_labels=ftract_labels)
     distance_matrix = roi_distances_from_centroids(centroids)
 
     return distance_matrix
 
 def dkt_roi_distances():
+    """
+    Load matrix of Euclidean distances for ROIs in DeskianKilliany parcellation.
 
+    Returns:
+    2D np.array of shape (n_roi,n_roi): distances between all pair of ROIs
+    """
     file = path("external/dk_parcellation_centroids/dk_centroids.csv")
     centroids = get_centroids_from_file(file,"geom")
     distance_matrix = roi_distances_from_centroids(centroids)
@@ -51,8 +99,14 @@ def dkt_roi_distances():
     return distance_matrix
 
 def schaefer_roi_distances():
+    """
+    Load matrix of Euclidean distances for ROIs in Schaefer200 parcellation.
+
+    Returns:
+    2D np.array of shape (n_roi,n_roi): distances between all pair of ROIs
+    """
     centroids_file = path('external/schaefer_parcellation_centroids/ROI_MAPPING_pytepfit.csv')
     centroids_mne = get_centroids_from_file(centroids_file,"geom_mne")
+    distance_matrix = roi_distances_from_centroids(centroids_mne)
 
-    ED = roi_distances_from_centroids(centroids_mne)
-    return ED
+    return distance_matrix
