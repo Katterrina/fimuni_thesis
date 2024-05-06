16b28cd7bd30c4259abc53442153342b27a7d845
Kateřina Č <katterrina@aurora>
2024-05-06 15:10:25 +0200

    docstring

--- src/save_and_load_averaged.py
+++ src/save_and_load_averaged.py
@@ -2,6 +2,17 @@ import os
 import numpy as np
 
 def load_averaged_matrix(path_to_dir,mode,matrix_name):
+    """
+    Load averaged matrix.
+
+    Parameters:
+    path_to_dir (str/path): path to data directory
+    mode (str): wich group averaging method was used to create the matrix
+    matrix_name (str): usually SC_W for weights or SC_L for lengths
+    
+    Returns:
+    2D np.array or None
+    """
     mean_file_path = path_to_dir + f"/{mode}_{matrix_name}.npy"
 
     if os.path.isfile(mean_file_path):
@@ -12,6 +23,14 @@ def load_averaged_matrix(path_to_dir,mode,matrix_name):
         return None
 
 def save_averaged_matrix(M,path_to_dir,mode,matrix_name):
+    """
+    Save averaged matrix.
+
+    Parameters:
+    path_to_dir (str/path): path to data directory
+    mode (str): wich group averaging method was used to create the matrix
+    matrix_name (str): usually SC_W for weights or SC_L for lengths
+    """
     mean_file_path = path_to_dir + f"/{mode}_{matrix_name}.npy"
 
     with open(mean_file_path,"xb") as f:
